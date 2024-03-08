49532944e74d8180f62bfc79bae445881dc72ea8
Bahman Movaqar <bahman@bahmanm.com>
2024-03-07 21:19:37 -0800

    Improve the output formatting

--- data-generator/src/main/groovy/stockperson/datagenerator/chapter_1_0/Main.groovy
+++ data-generator/src/main/groovy/stockperson/datagenerator/chapter_1_0/Main.groovy
@@ -31,12 +31,19 @@ class Main {
         header: 'StockPerson Data Generator - Chapter 1.0',
         usage: 'Main [OPTION1] [OPTION2] ...'
         )
-    cli._(longOpt: 'invoice-count', args: 1, argName: 'N', required: true,
-    'How many invoices to generate?')
+    cli._(longOpt: 'invoice-count', args: 1, argName: 'N', required: false,
+          'How many invoices to generate?')
+    cli._(longOpt: 'invoice-file', args: 1, argName: 'file', required: false,
+          'Path (absolute or relative) to the invoices file.')
+    cli.h(longOpt: 'help', args: 0, required: false, 'Show this help.')
   }
 
   static void main(String[] args) {
     def options = cli.parse(args); assert options
+    if (options.help) {
+      cli.usage()
+      return
+    }
 
     def countProducts = RandomNumber.instance.anInt(10, 50)
     def countCustomers = RandomNumber.instance.anInt(10, 100)
@@ -48,8 +55,14 @@ class Main {
     def customers = (0..10).collect { RandomString.instance.alphabetic(5, 10) }
     def invoices = InvoiceService.instance.make(products, customers, countInvoices)
 
+    def invoiceFilepath = options.'invoice-file' ?: 'stockperson-chapter-1.0-sales-invoices.csv'
+    def invoiceFile = new File(invoiceFilepath)
+    invoiceFile << '# Produced using StockPerson Data Generator (chapter 1.0)\n'
+    invoiceFile << '# https://github.com/bahmanm/stockperson/\n'
+    invoiceFile << '# docNo,customer,date,total,discount,lineNo,product,qty,price,lineAmt\n'
     invoices.each { inv ->
-      println(InvoiceService.instance.toCsv(inv).join('\n'))
+      invoiceFile << InvoiceService.instance.toCsv(inv).join('\n')
     }
+    invoiceFile << '\n'
   }
 }
